de1dd28e18a488beed6dbb8b653ccc05b46671a2
Bobby1 <bwen@yahoo.com>
2025-03-17 11:01:09 -0700

    New test branch

--- service.tf
+++ service.tf
@@ -1,16 +1,19 @@
 resource "aws_ecs_service" "service_name" {
-  name            = "${var.business_division}-${var.environment}-ecs-service"
+  name            = "${var.environment}-${var.service}-ecs-service"
   cluster         = aws_ecs_cluster.ecs-cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = var.app_count[var.environment]
-  launch_type     = "FARGATE"
+  # launch_type     = "FARGATE"
+  launch_type = "EC2"
   network_configuration {
-    security_groups = [aws_security_group.ecs_tasks2.id]
+    security_groups = [aws_security_group.ecs_tasks.id]
     subnets         = aws_subnet.private.*.id
   }
   load_balancer {
     target_group_arn = aws_lb_target_group.ecs-fargate-TG.arn
     # container_name   = "devops-webpage"
+    # container_name = "${var.environment}-${var.service}-LB"
+    # container_name = "${var.business_division}-${var.environment}-cluster-alb"
     container_name = "${var.business_division}-${var.environment}-webpage"
     container_port = 80
   }
--- variables.tf
+++ variables.tf
@@ -14,6 +14,10 @@ variable "region" {
   description = "Region in which AWS Resources to be created"
   type        = string
 }
+variable "service" {
+  description = "Service name"
+  type        = string
+}
 variable "app_count" {
   description = "Number of instances to provision."
   type        = map(number)
@@ -38,6 +42,7 @@ variable "ingress_cidr_blocks" {
   default = {
     ### IP for individual developer's remote address, 67.174.209.57/32 is an access IP address  ### DEBUG
     dev = ["98.207.22.120/32", "67.180.167.206/32", "75.36.1.193/32"]
+    # dev = ["0.0.0.0/0",]
     ### example IPs for a company's testing evironement  ### DEBUG
     stg = ["98.207.22.120/32", "172.25.176.0/24", "172.31.0.0/16", ]
     prd = ["0.0.0.0/0", ]
